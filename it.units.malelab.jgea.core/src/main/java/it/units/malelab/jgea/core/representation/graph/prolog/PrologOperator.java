7d4f401ec233b8ef015989e74aed9934e860bc0a
Simone Pozzi <Pozzi.s1317@gmail.com>
2022-11-21 17:40:29 +0100

    New class

---
+++ it.units.malelab.jgea.core/src/main/java/it/units/malelab/jgea/core/representation/graph/prolog/PrologOperator.java
@@ -0,0 +1,24 @@
+package it.units.malelab.jgea.core.representation.graph.prolog;
+
+import it.units.malelab.jgea.core.operator.Mutation;
+
+import java.util.List;
+import java.util.random.RandomGenerator;
+
+public class PrologOperator implements Mutation<PrologGraph> {
+
+  private final String operatorDescription;
+  private final List<String> domainDefinition;
+  private final List<String> structuralRules;
+
+  public PrologOperator(String operatorDescription, List<String> domainDefinition, List<String> domainStructuralRules) {
+    this.operatorDescription = operatorDescription;
+    this.domainDefinition = domainDefinition;
+    this.structuralRules = domainStructuralRules;
+  }
+
+  @Override
+  public PrologGraph mutate(PrologGraph graph, RandomGenerator random) {
+    return PrologGraphUtils.applyOperator(operatorDescription, graph, domainDefinition, structuralRules);
+  }
+}
