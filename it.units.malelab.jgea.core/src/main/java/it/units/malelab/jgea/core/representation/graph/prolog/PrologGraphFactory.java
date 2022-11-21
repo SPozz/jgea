6bffddf47519aaeda97630be695a99068b87815d
Simone Pozzi <Pozzi.s1317@gmail.com>
2022-11-21 10:31:53 +0100

    Added analysis

--- it.units.malelab.jgea.core/src/main/java/it/units/malelab/jgea/core/representation/graph/prolog/PrologGraphFactory.java
+++ it.units.malelab.jgea.core/src/main/java/it/units/malelab/jgea/core/representation/graph/prolog/PrologGraphFactory.java
@@ -2,7 +2,13 @@ package it.units.malelab.jgea.core.representation.graph.prolog;
 
 import it.units.malelab.jgea.core.Factory;
 import it.units.malelab.jgea.core.util.Misc;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.random.RandomGenerator;
 
@@ -50,5 +56,37 @@ public class PrologGraphFactory implements Factory<PrologGraph> {
     return graphList;
   }
 
+
+  void exportFactoryAnalysis(String domainName, int minDimension, int maxDimension, PrologGraph originGraph, List<String> operators, List<String> domainDefinition, List<String> structuralRules) {
+    final int nGraphs = 200;
+    final Random random = new Random();
+
+    List<PrologGraph> graphs = new PrologGraphFactory(minDimension, maxDimension, originGraph, operators, domainDefinition, structuralRules).build(nGraphs, random);
+
+    String fileName = domainName + minDimension + "To" + maxDimension;
+
+    try {
+      // create a writer
+      Writer writer = Files.newBufferedWriter(Paths.get("C:\\Users\\Simone\\Desktop\\GitHub_Tesi\\jgea_data\\sizeDistribution\\" + fileName));
+
+      // write CSV file
+      CSVPrinter printer = CSVFormat.DEFAULT.withHeader("graph", "dimension", "nNodes", "nArcs").print(writer);
+      int index = 0;
+
+      for (PrologGraph graph : graphs) {
+        printer.printRecord(index, graph.size(), graph.nodes().size(), graph.arcs().size());
+        index++;
+      }
+
+      // flush the stream
+      printer.flush();
+
+      // close the writer
+      writer.close();
+
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
 
 }
