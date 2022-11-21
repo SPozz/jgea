488c9bf31201b66727ef33cc6a91eb663f5061c9
Simone Pozzi <Pozzi.s1317@gmail.com>
2022-11-21 16:14:16 +0100

    Added domainDef and structRules (exmp)

--- it.units.malelab.jgea.sample/src/main/java/it/units/malelab/jgea/sample/lab/prolog/FirstExample.java
+++ it.units.malelab.jgea.sample/src/main/java/it/units/malelab/jgea/sample/lab/prolog/FirstExample.java
@@ -23,15 +23,19 @@ import java.util.logging.Logger;
 import static it.units.malelab.jgea.sample.lab.TuiExample.*;
 
 public class FirstExample implements Runnable {
-
-  // Basic_functions and double_functions from TuiExample
   private final static Logger L = Logger.getLogger(TuiExample.class.getName());
   private final ExecutorService executorService;
   private final int minDim;
   private final int maxDim;
-  private final List<String> domainDefinition = new ArrayList<>();
+  private final List<String> domainDefinition = Arrays.asList(
+          ":- dynamic node_id/1.",
+          ":- dynamic start/2.",
+          ":- dynamic type/2.",
+          ":- dynamic value/2.",
+          ":- dynamic edge_id/1.",
+          ":- dynamic edge/3.");
   private final List<String> structuralRules;
-  private final PrologGraph originGraph = new PrologGraph();
+  private final PrologGraph originGraph;
   private final List<String> operators = new ArrayList<>();
 
   public FirstExample(int minDim, int maxDim, List<String> structuralRules) {
@@ -39,11 +43,58 @@ public class FirstExample implements Runnable {
     this.minDim = minDim;
     this.maxDim = maxDim;
     this.structuralRules = structuralRules;
+
+    PrologGraph origin = new PrologGraph();
+    LinkedHashMap<String, Object> node1 = new LinkedHashMap<>();
+    node1.put("node_id", "first");
+    node1.put("start", 1);
+    node1.put("type", "operator");
+    node1.put("value", "+");
+    LinkedHashMap<String, Object> node2 = new LinkedHashMap<>();
+    node2.put("node_id", "second");
+    node2.put("start", 0);
+    node2.put("type", "variable");
+    node2.put("value", 5);
+    LinkedHashMap<String, Object> node3 = new LinkedHashMap<>();
+    node3.put("node_id", "third");
+    node3.put("start", 0);
+    node3.put("type", "variable");
+    node3.put("value", 3);
+    LinkedHashMap<String, Object> edge1 = new LinkedHashMap<>();
+    edge1.put("edge_id", "firstEdge");
+    LinkedHashMap<String, Object> edge2 = new LinkedHashMap<>();
+    edge2.put("edge_id", "secondEdge");
+    origin.addNode(node1);
+    origin.addNode(node2);
+    origin.addNode(node3);
+    origin.setArcValue(node2, node1, edge1);
+    origin.setArcValue(node3, node1, edge2);
+    this.originGraph = origin;
   }
 
 
   public static void main(String[] args) {
-    new FirstExample(10, 20, Arrays.asList("", "")).run();
+    List<String> structuralRules = Arrays.asList(
+            "variable_val(X) :- integer(X), X>= 0, X < 10.",
+            "operator_val(+).",
+            "operator_val(*).",
+            "operator_val(-).",
+            "operator_val(/).",
+            "start_outdegree(S) :- findall(E, edge(S,_,E), RES), length(RES,N1), N1 == 0.",
+            "node_outdegree(S) :- findall(E, edge(S,_,E), RES), length(RES,N1), N1 == 1.",
+            "operator_indegree(T) :- findall(E, edge(_,T,E), RES), length(RES,N1), N1 == 2.",
+            "variable_indegree(T) :- findall(E, edge(_,T,E), RES), length(RES,N1), N1 == 0.",
+            "check_start :- findall(N,start(N,1), N),length(N,N1), N1 == 1.",
+            "start_connected(N) :- start(N,1).",
+            "start_connected(N) :- edge(N,X,_), start_connected(X).",
+            "is_valid :- check_start," +
+                    "    foreach(findall(N,node_id(N),Node),maplist(start_connected,Node))," +
+                    "    foreach(findall(T,(node_id(T),start(T,1)),T), maplist(start_outdegree,T))," +
+                    "    foreach(findall(T,(node_id(T),start(T,0)),T), maplist(node_outdegree,T))," +
+                    "    foreach(findall(O,type(O,operator),O), maplist(operator_indegree,O))," +
+                    "    foreach(findall(V,type(V,variable),V), maplist(variable_indegree,V)).");
+
+    new FirstExample(10, 30, structuralRules).run();
   }
 
 
