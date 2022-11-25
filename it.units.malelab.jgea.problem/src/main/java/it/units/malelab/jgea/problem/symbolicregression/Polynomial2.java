9bfbff6ea76eadf85137c0593c2f2bc982baa216
Simone Pozzi <Pozzi.s1317@gmail.com>
2022-11-25 10:36:53 +0100

    Added new Problem

---
+++ it.units.malelab.jgea.problem/src/main/java/it/units/malelab/jgea/problem/symbolicregression/Polynomial2.java
@@ -0,0 +1,19 @@
+package it.units.malelab.jgea.problem.symbolicregression;
+
+public class Polynomial2 extends SyntheticSymbolicRegressionProblem {
+
+
+    public Polynomial2 (SymbolicRegressionFitness.Metric metric){
+      super(
+              v -> {
+                double x = v[0];
+                return x * x + x;
+              },
+              MathUtils.pairwise(MathUtils.equispacedValues(-1, 1, .01)),
+              MathUtils.pairwise(MathUtils.equispacedValues(-1, 1, .01)),
+              metric
+      );
+    }
+
+
+}
