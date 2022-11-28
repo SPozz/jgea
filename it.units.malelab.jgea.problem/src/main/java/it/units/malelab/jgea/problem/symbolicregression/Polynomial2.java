5572a9d1aa034072f1a5e1ec0173436ac2bb1eb5
Simone Pozzi <Pozzi.s1317@gmail.com>
2022-11-28 10:45:07 +0100

    + 1.0d

--- it.units.malelab.jgea.problem/src/main/java/it/units/malelab/jgea/problem/symbolicregression/Polynomial2.java
+++ it.units.malelab.jgea.problem/src/main/java/it/units/malelab/jgea/problem/symbolicregression/Polynomial2.java
@@ -3,17 +3,17 @@ package it.units.malelab.jgea.problem.symbolicregression;
 public class Polynomial2 extends SyntheticSymbolicRegressionProblem {
 
 
-    public Polynomial2 (SymbolicRegressionFitness.Metric metric){
-      super(
-              v -> {
-                double x = v[0];
-                return x * x + x;
-              },
-              MathUtils.pairwise(MathUtils.equispacedValues(-1, 1, .01)),
-              MathUtils.pairwise(MathUtils.equispacedValues(-1, 1, .01)),
-              metric
-      );
-    }
+  public Polynomial2(SymbolicRegressionFitness.Metric metric) {
+    super(
+            v -> {
+              double x = v[0];
+              return x * x + x + 1.0d;
+            },
+            MathUtils.pairwise(MathUtils.equispacedValues(-1, 1, .01)),
+            MathUtils.pairwise(MathUtils.equispacedValues(-1, 1, .01)),
+            metric
+    );
+  }
 
 
 }
