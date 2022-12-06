2c76b083a39baac27ce7a7f3bced0ad75f75b6e9
Simone Pozzi <Pozzi.s1317@gmail.com>
2022-12-06 10:42:42 +0100

    Reformat

--- it.units.malelab.jgea.sample/src/main/java/it/units/malelab/jgea/sample/lab/prolog/TreeRegressionRunnable.java
+++ it.units.malelab.jgea.sample/src/main/java/it/units/malelab/jgea/sample/lab/prolog/TreeRegressionRunnable.java
@@ -262,9 +262,9 @@ public class TreeRegressionRunnable implements Runnable {
     final double minConst = 0;
     final double maxConst = 2.0d;
     final int nInput = 1;
-    structuralRules.add(0,"n_input("+nInput+").");
-    structuralRules.add(0,"max_const("+maxConst+").");
-    structuralRules.add(0,"min_const("+minConst+").");
+    structuralRules.add(0, "n_input(" + nInput + ").");
+    structuralRules.add(0, "max_const(" + maxConst + ").");
+    structuralRules.add(0, "min_const(" + minConst + ").");
 
     new TreeRegressionRunnable(5, 29, factoryOperators, operators, structuralRules).run();
 
