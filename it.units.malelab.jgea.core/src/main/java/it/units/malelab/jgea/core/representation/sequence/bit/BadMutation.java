a1a56febe129ac78983d91e5b5227acc0f64737f
Simone Pozzi <Pozzi.s1317@gmail.com>
2023-01-10 12:57:40 +0100

    Added

---
+++ it.units.malelab.jgea.core/src/main/java/it/units/malelab/jgea/core/representation/sequence/bit/BadMutation.java
@@ -0,0 +1,17 @@
+package it.units.malelab.jgea.core.representation.sequence.bit;
+
+import it.units.malelab.jgea.core.operator.Mutation;
+
+import java.util.random.RandomGenerator;
+
+public class BadMutation implements Mutation<BitString> {
+
+  @Override
+  public BitString mutate(BitString parent, RandomGenerator random) {
+    BitString newG = BitString.copyOf(parent);
+    int index = random.nextInt(newG.size());
+    newG.set(index, false);
+    return newG;
+  }
+
+}
