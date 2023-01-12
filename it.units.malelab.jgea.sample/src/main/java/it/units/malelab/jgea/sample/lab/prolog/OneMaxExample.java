d1e1ca628ca13c127a7730a3afd31ab710480477
Simone Pozzi <Pozzi.s1317@gmail.com>
2023-01-12 18:04:03 +0100

    Upgraded

--- it.units.malelab.jgea.sample/src/main/java/it/units/malelab/jgea/sample/lab/prolog/OneMaxExample.java
+++ it.units.malelab.jgea.sample/src/main/java/it/units/malelab/jgea/sample/lab/prolog/OneMaxExample.java
@@ -4,7 +4,6 @@ import it.units.malelab.jgea.core.QualityBasedProblem;
 import it.units.malelab.jgea.core.listener.ListenerFactory;
 import it.units.malelab.jgea.core.listener.NamedFunction;
 import it.units.malelab.jgea.core.listener.TabularPrinter;
-import it.units.malelab.jgea.core.operator.GeneticOperator;
 import it.units.malelab.jgea.core.representation.sequence.UniformCrossover;
 import it.units.malelab.jgea.core.representation.sequence.bit.*;
 import it.units.malelab.jgea.core.selector.Last;
@@ -12,7 +11,6 @@ import it.units.malelab.jgea.core.selector.Tournament;
 import it.units.malelab.jgea.core.solver.*;
 import it.units.malelab.jgea.core.solver.state.POSetPopulationState;
 import it.units.malelab.jgea.core.util.Misc;
-import it.units.malelab.jgea.core.util.TextPlotter;
 import it.units.malelab.jgea.problem.synthetic.OneMax;
 import it.units.malelab.jgea.sample.Worker;
 
@@ -63,7 +61,18 @@ public class OneMaxExample extends Worker {
 
   @Override
   public void run() {
-    int size = 1000;
+    int maxIterations = 30;
+    int nPop = 100;
+    int maxDiversityAttempts = 100;
+    int nTournament = 5;
+
+    //TODO: cambiare operatori
+    //TODO: aggiunere nSeed
+    //TODO: export CSV
+
+    int size = 1000; //TODO: iterare con 100-1000-5000
+
+
     Random r = new Random(1);
     QualityBasedProblem<BitString, Double> p = new OneMax();
     List<NamedFunction<? super POSetPopulationState<?, ?, ?>, ?>> keysFunctions = List.of();
@@ -76,100 +85,53 @@ public class OneMaxExample extends Worker {
     List<IterativeSolver<? extends POSetPopulationState<?, BitString, Double>, QualityBasedProblem<BitString, Double>
             , BitString>> solvers = new ArrayList<>();
 
+    Function<Long, Double> constantSchedulNull = x -> 0.0d;
+    Function<Long, Double> constantScheduleHigh = x -> 0.025d;
+    Function<Long, Double> constantScheduleLow = x -> 0.001d;
+    Function<Long, Double> stepScheduleInit = x -> x < maxIterations / 2 ? 0.01 : 0;
+    Function<Long, Double> stepScheduleEnd = x -> x > maxIterations / 2 ? 0.01 : 0;
+    List<Function<Long, Double>> schedules = Arrays.asList(constantScheduleHigh, constantScheduleLow, stepScheduleInit, stepScheduleEnd, constantSchedulNull);
 
-    int maxIterations = 100;
-    Function<Long, Double> constantPerturbationHigh = x -> 0.025d;
-    Function<Long, Double> constantPerturbationLow = x -> 0.001d;
-    Function<Long, Double> constantPerturbationStepInit = x -> x < maxIterations / 2 ? 0.01 : 0;
-    Function<Long, Double> constantPerturbationStepEnd = x -> x > maxIterations / 2 ? 0.01 : 0;
 
     solvers.add(new StandardWithEnforcedDiversityEvolver<POSetPopulationState<BitString, BitString, Double>,
             QualityBasedProblem<BitString, Double>, BitString, BitString, Double>(
             Function.identity(),
             new BitStringFactory(size),
-            100,
+            nPop,
             StopConditions.targetFitness(0d).or(StopConditions.nOfIterations(maxIterations)),
             Map.of(new UniformCrossover<>(new BitStringFactory(size)), 0.8d, new BitFlipMutation(0.01d), 0.2d),
-            new Tournament(5),
+            new Tournament(nTournament),
             new Last(),
-            100,
+            nPop,
             true,
             false,
             (problem, random) -> new POSetPopulationState<>(),
-            100
+            maxDiversityAttempts
     ));
 
+    for (Function<Long, Double> schedule : schedules) {
+      solvers.add(
+              new AdaptiveEvolver<>(
+                      Function.identity(),
+                      new BitStringFactory(size),
+                      nPop,
+                      StopConditions.targetFitness(0d).or(StopConditions.nOfIterations(maxIterations)),
+                      Map.ofEntries(
+                              Map.entry(new UniformCrossover<>(new BitStringFactory(size)), 1d),
+                              Map.entry(new BitFlipMutation(0.01d), 1d),
+                              Map.entry(new BadMutation(), 1d),
+                              Map.entry(new GoodMutation(), 1d)
+                      ),
+                      new Tournament(nTournament),
+                      new Last(),
+                      nPop,
+                      true,
+                      false,
+                      maxDiversityAttempts,
+                      schedule
+              ));
+    }
 
-    Map<GeneticOperator<BitString>, Double> operatorsProbBase = new HashMap<>();
-    operatorsProbBase.put(new UniformCrossover<>(new BitStringFactory(size)), 0.5d);
-    operatorsProbBase.put(new BitFlipMutation(0.01d), 0.5d);
-
-//    solvers.add(
-    AdaptiveEvolver adaptiveEvolver2 = new AdaptiveEvolver<>(
-
-            Function.identity(),
-            new BitStringFactory(size),
-            100,
-            StopConditions.targetFitness(0d).or(StopConditions.nOfIterations(maxIterations)),
-            operatorsProbBase,
-            new Tournament(5),
-            new Last(),
-            100,
-            true,
-            false,
-            100,
-            constantPerturbationHigh);
-    solvers.add(adaptiveEvolver2);
-
-    Map<GeneticOperator<BitString>, Double> operatorsProb3 = new HashMap<>();
-    operatorsProb3.put(new UniformCrossover<>(new BitStringFactory(size)), 0.33d);
-    operatorsProb3.put(new BitFlipMutation(0.01d), 0.33d);
-    operatorsProb3.put(new BadMutation(), 0.33d);
-//    solvers.add(
-    AdaptiveEvolver adaptiveEvolver3 = new AdaptiveEvolver<>(
-            Function.identity(),
-            new BitStringFactory(size),
-            100,
-            StopConditions.targetFitness(0d).or(StopConditions.nOfIterations(maxIterations)),
-            operatorsProb3,
-            new Tournament(5),
-            new Last(),
-            100,
-            true,
-            false,
-            100,
-            constantPerturbationHigh
-    );
-    solvers.add(adaptiveEvolver3);
-
-
-    Map<GeneticOperator<BitString>, Double> operatorsProb4 = new HashMap<>();
-    operatorsProb4.put(new UniformCrossover<>(new BitStringFactory(size)), 0.25d);
-    operatorsProb4.put(new BitFlipMutation(0.01d), 0.25d);
-    operatorsProb4.put(new BadMutation(), 0.25d);
-    operatorsProb4.put(new GoodMutation(), 0.25d);
-
-    AdaptiveEvolver adaptiveEvolver4 =
-            new AdaptiveEvolver<>(
-                    Function.identity(),
-                    new BitStringFactory(size),
-                    100,
-                    StopConditions.targetFitness(0d).or(StopConditions.nOfIterations(maxIterations)),
-                    Map.ofEntries(
-//                            Map.entry(new UniformCrossover<>(new BitStringFactory(size)), 1d),
-                            Map.entry(new BitFlipMutation(0.01d), 1d),
-                            Map.entry(new BadMutation(), 1d),
-                            Map.entry(new GoodMutation(), 1d)
-                    ),
-                    new Tournament(5),
-                    new Last(),
-                    100,
-                    true,
-                    false,
-                    100,
-                    constantPerturbationHigh
-            );
-    solvers.add(adaptiveEvolver4);
 
     for (IterativeSolver<? extends POSetPopulationState<?, BitString, Double>, QualityBasedProblem<BitString, Double>
             , BitString> evolver : solvers) {
