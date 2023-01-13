d0879d2d204b11d9adf744b06e02a99ffcb5e0c5
Simone Pozzi <Pozzi.s1317@gmail.com>
2023-01-13 12:15:22 +0100

    Completed_size200

--- it.units.malelab.jgea.sample/src/main/java/it/units/malelab/jgea/sample/lab/prolog/OneMaxExample.java
+++ it.units.malelab.jgea.sample/src/main/java/it/units/malelab/jgea/sample/lab/prolog/OneMaxExample.java
@@ -1,6 +1,8 @@
 package it.units.malelab.jgea.sample.lab.prolog;
 
+import com.google.common.base.Stopwatch;
 import it.units.malelab.jgea.core.QualityBasedProblem;
+import it.units.malelab.jgea.core.listener.CSVPrinter;
 import it.units.malelab.jgea.core.listener.ListenerFactory;
 import it.units.malelab.jgea.core.listener.NamedFunction;
 import it.units.malelab.jgea.core.listener.TabularPrinter;
@@ -10,20 +12,23 @@ import it.units.malelab.jgea.core.selector.Last;
 import it.units.malelab.jgea.core.selector.Tournament;
 import it.units.malelab.jgea.core.solver.*;
 import it.units.malelab.jgea.core.solver.state.POSetPopulationState;
-import it.units.malelab.jgea.core.util.Misc;
 import it.units.malelab.jgea.problem.synthetic.OneMax;
 import it.units.malelab.jgea.sample.Worker;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
 import static it.units.malelab.jgea.core.listener.NamedFunctions.*;
 import static it.units.malelab.jgea.core.listener.NamedFunctions.attribute;
+import static it.units.malelab.jgea.sample.Args.i;
+import static it.units.malelab.jgea.sample.Args.ri;
 
 public class OneMaxExample extends Worker {
 
-  public final static List<NamedFunction<? super POSetPopulationState<?, ?, ?>, ?>> BASIC_FUNCTIONS =
+  public final static List<NamedFunction<? super POSetPopulationState<?, ?, ? extends Double>, ?>> functions =
           List.of(
                   iterations(),
                   births(),
@@ -41,11 +46,7 @@ public class OneMaxExample extends Worker {
                   uniqueness().of(each(fitness())).of(all()),
                   size().of(genotype()).of(best()),
                   size().of(solution()).of(best()),
-                  fitnessMappingIteration().of(best())
-          );
-
-  public final static List<NamedFunction<? super POSetPopulationState<?, ?, ? extends Double>, ?>> DOUBLE_FUNCTIONS =
-          List.of(
+                  fitnessMappingIteration().of(best()),
                   fitness().reformat("%5.3f").of(best()),
                   hist(8).of(each(fitness())).of(all()),
                   max(Double::compare).reformat("%5.3f").of(each(fitness())).of(all())
@@ -61,37 +62,41 @@ public class OneMaxExample extends Worker {
 
   @Override
   public void run() {
-    int maxIterations = 50;
-    int nPop = 100;
-    int maxDiversityAttempts = 100;
+    int nPop = i(a("nPop", "100"));
     int nTournament = 5;
-
-    //TODO: cambiare operatori
-    //TODO: aggiunere nSeed
-    //TODO: export CSV
-
-    int size = 1000; //TODO: iterare con 100-1000-5000
-
+    int maxDiversityAttempts = 100;
+    int maxIterations = i(a("nIterations", "50"));
+    int[] seeds = ri(a("seed", "0:100"));
+    int size = i(a("size", "200"));
 
     Random r = new Random(1);
     QualityBasedProblem<BitString, Double> p = new OneMax();
-    List<NamedFunction<? super POSetPopulationState<?, ?, ?>, ?>> keysFunctions = List.of();
+
+    List<NamedFunction<? super Map<String, Object>, ?>> kFunctions = List.of(
+            attribute("seed").reformat("%2d"),
+            attribute("size").reformat("%2d"),
+            attribute("evolver").reformat("%20.20s")
+    );
     ListenerFactory<POSetPopulationState<?, ?, ? extends Double>, Map<String, Object>> listenerFactory =
-            ListenerFactory.all(
-                    List.of(new TabularPrinter<>(
-                            Misc.concat(List.of(keysFunctions, BASIC_FUNCTIONS, DOUBLE_FUNCTIONS)),
-                            List.of(attribute("solver"))
-                    )));
+            new TabularPrinter<>(
+                    functions,
+                    kFunctions
+            );
+
+    listenerFactory = ListenerFactory.all(List.of(
+            listenerFactory,
+            new CSVPrinter<>(functions, kFunctions, new File("./prolog/results/oneMax-" + size + ".csv"))
+    ));
+
+
     List<IterativeSolver<? extends POSetPopulationState<?, BitString, Double>, QualityBasedProblem<BitString, Double>
             , BitString>> solvers = new ArrayList<>();
-
     Function<Long, Double> constantSchedulNull = x -> 0.0d;
     Function<Long, Double> constantScheduleHigh = x -> 0.025d;
     Function<Long, Double> constantScheduleLow = x -> 0.001d;
     Function<Long, Double> stepScheduleInit = x -> x < maxIterations / 2 ? 0.01 : 0;
     Function<Long, Double> stepScheduleEnd = x -> x > maxIterations / 2 ? 0.01 : 0;
     List<Function<Long, Double>> schedules = Arrays.asList(constantScheduleHigh, constantScheduleLow, stepScheduleInit, stepScheduleEnd, constantSchedulNull);
-
 
     solvers.add(new StandardWithEnforcedDiversityEvolver<POSetPopulationState<BitString, BitString, Double>,
             QualityBasedProblem<BitString, Double>, BitString, BitString, Double>(
@@ -133,23 +138,41 @@ public class OneMaxExample extends Worker {
     }
 
 
-    for (IterativeSolver<? extends POSetPopulationState<?, BitString, Double>, QualityBasedProblem<BitString, Double>
-            , BitString> evolver : solvers) {
-      try {
-        Collection<BitString> solutions = evolver.solve(
-                p,
-                r,
-                executorService,
-                listenerFactory.build(Map.of("solver", evolver.getClass().getSimpleName())).deferred(executorService)
-        );
-        System.out.printf("Found %d solutions with " + "%s.%n", solutions.size(), evolver.getClass().getSimpleName());
-      } catch (SolverException e) {
-        e.printStackTrace();
+    for (int seed : seeds) {
+      for (IterativeSolver<? extends POSetPopulationState<?, BitString, Double>, QualityBasedProblem<BitString, Double>
+              , BitString> evolver : solvers) {
+        try {
+          Stopwatch stopwatch = Stopwatch.createStarted();
+          Map<String, Object> keys = Map.ofEntries(
+                  Map.entry("seed", seed),
+                  Map.entry("evolver", evolver.getClass().getSimpleName()),
+                  Map.entry("size", size)
+          );
+
+          L.info(String.format("Starting %s", keys));
+
+          Collection<BitString> solutions = evolver.solve(
+                  p,
+                  r,
+                  executorService,
+                  listenerFactory.build(keys).deferred(executorService)
+          );
+          L.info(String.format(
+                  "Done %s: %d solutions in %4.1fs",
+                  keys,
+                  solutions.size(),
+                  (double) stopwatch.elapsed(TimeUnit.MILLISECONDS) / 1000d
+          ));
+        } catch (SolverException e) {
+          e.printStackTrace();
+        }
       }
     }
 
 
     listenerFactory.shutdown();
+
+
   }
 }
 
