336bb6657e1ad7c43e12f25d9e0117a9f95c2d69
Simone Pozzi <Pozzi.s1317@gmail.com>
2023-01-19 17:43:33 +0100

    RollBack

--- it.units.malelab.jgea.core/src/main/java/it/units/malelab/jgea/core/solver/AdaptiveEvolver.java
+++ it.units.malelab.jgea.core/src/main/java/it/units/malelab/jgea/core/solver/AdaptiveEvolver.java
@@ -60,8 +60,7 @@ public class AdaptiveEvolver<P extends QualityBasedProblem<S, Q>, G, S, Q> exten
           Factory<? extends G> genotypeFactory,
           int populationSize,
           Predicate<? super AdaptiveEvolver.State<G, S, Q>> stopCondition,
-//          Map<GeneticOperator<G>, Double> operators,
-          TreeMap<GeneticOperator<G>, Double> operators,
+          Map<GeneticOperator<G>, Double> operators,
           Selector<? super Individual<? super G, ? super S, ? super Q>> parentSelector,
           Selector<? super Individual<? super G, ? super S, ? super Q>> unsurvivalSelector,
           int offspringSize,
@@ -122,9 +121,7 @@ public class AdaptiveEvolver<P extends QualityBasedProblem<S, Q>, G, S, Q> exten
     double epsilon = Math.min(Math.abs(probabilityVariationSchedule.apply(state.getNOfIterations())), 1d);
     for (Map.Entry<GeneticOperator<G>, Double> change : changes) {
       GeneticOperator<G> operator = change.getKey();
-//      state.getOperators().put(operator, (1.0d + (change.getValue() > 0 ? 1d : -1d) * epsilon) * state.getOperators().get(operator));
-
-      state.getOperators().put(operator, (1.0d + change.getValue() * epsilon) * state.getOperators().get(operator)); //TODO: change here
+      state.getOperators().put(operator, (1.0d + (change.getValue() > 0 ? 1d : -1d) * epsilon) * state.getOperators().get(operator));
     }
     return offspring;
   }
@@ -163,7 +160,6 @@ public class AdaptiveEvolver<P extends QualityBasedProblem<S, Q>, G, S, Q> exten
         nAfter++;
       }
     }
-//    return nAfter - nBefore;
-    return ((double) nAfter - nBefore) / outcomes.size(); //TODO: change here
+    return nAfter - nBefore;
   }
 }
